2201fd4c2e7d5d9e34b84dde5c2dee2b1c0f0399
Black-Lights <mohammadammar.mughees@mail.polimi.it>
2024-07-13 20:40:20 +0200

    reduced the sizes of the images

--- pages/Contact-us.vue
+++ pages/Contact-us.vue
@@ -29,9 +29,7 @@ export default {
 
 <style scoped>
 .contact-section {
-  /* background: url('https://plus.unsplash.com/premium_photo-1661763911173-f2f7becc70b0?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center fixed;*/
-  /* background: url('https://images.unsplash.com/photo-1534536281715-e28d76689b4d?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center fixed;*/
-  background: url('https://images.unsplash.com/photo-1596524430615-b46475ddff6e?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center fixed; background-size: cover;
+  background: url('static/img_contact_us.jpg') no-repeat center center fixed; background-size: cover;
   padding: 40px;
   margin: 20px 0; /* Add some margin for spacing */
 
